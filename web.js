f7cfbe7d9a1f7d80e4c633ba53b0e3609c03a6d3
Ubuntu <ubuntu@ip-172-31-35-49.us-west-2.compute.internal>
2013-07-11 00:31:07 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env node
 var fs = require('fs');
 buf = new Buffer(256);
@@ -7,7 +8,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync("/bitstarter/index.html")));
+  response.send(buf.toString(fs.readFileSync("bitstarter/index.html")));
 });
 
 var port = process.env.PORT || 5000;
